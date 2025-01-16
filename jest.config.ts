58b8c3e5be8d461104b22299bff8cd31426bc13a
Paul Gottschling <paul.gottschling@goteleport.com>
2025-01-16 11:57:35 -0500

    Start fresh

--- jest.config.ts
+++
@@ -1,9 +0,0 @@
-import type { Config } from "jest";
-
-const config: Config = {
-  transform: {
-    "\\.[tj]sx?$": ["babel-jest", { configFile: "./babel.jest.config.json" }],
-  },
-};
-
-export default config;
