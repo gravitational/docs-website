1ba364bbddbecc64ca03bdada57633ddf9751ff8
Paul Gottschling <paul.gottschling@goteleport.com>
2025-01-16 10:46:54 -0500

    Use a JSON file for Babel

--- babel.jest.config.js
+++
@@ -1,5 +0,0 @@
-export {
-  presets: [
-    "@babel/preset-typescript",
-  ],
-};
--- jest.config.ts
+++ jest.config.ts
@@ -2,7 +2,7 @@ import type { Config } from "jest";
 
 const config: Config = {
   transform: {
-    "\\.[tj]sx?$": ["babel-jest", { configFile: "./babel.jest.config.js" }],
+    "\\.[tj]sx?$": ["babel-jest", { configFile: "./babel.jest.config.json" }],
   },
 };
 
