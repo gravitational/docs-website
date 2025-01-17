8657a46f86f04e7ff8e75b5504897c354a933d96
Paul Gottschling <paul.gottschling@goteleport.com>
2025-01-17 16:55:09 -0500

    Get the Jest config to work

--- jest.config.js
+++ jest.config.js
@@ -1,12 +1,16 @@
+/** @type {import('ts-jest').JestConfigWithTsJest} */
 const jestConfig = {
-  preset: "ts-jest",
+  preset: "ts-jest/presets/default-esm",
   testEnvironment: "node",
   extensionsToTreatAsEsm: [".ts"],
   transform: {
     "^.+\\.[tj]s$": [
       "ts-jest",
+      {
+        useESM: true,
+      },
     ],
   },
-  transformIgnorePatterns: [""],
+  transformIgnorePatterns: ["<rootDir>/node_modules/*"],
 };
 export default jestConfig;
