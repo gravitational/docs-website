779668473469fda4eaf29c2de906b9a7f30e2387
Paul Gottschling <paul.gottschling@goteleport.com>
2025-01-17 11:12:33 -0500

    More tweaks
    Use the default ts-jest preset in tsconfig
    
    Restore the experimental modules node option

--- jest.config.js
+++ jest.config.js
@@ -1,11 +1,12 @@
-// Config pasted from:
-// https://kulshekhar.github.io/ts-jest/docs/guides/esm-support/
-import { createDefaultEsmPreset } from "ts-jest";
-
-const presetConfig = createDefaultEsmPreset();
-
 const jestConfig = {
-  ...presetConfig,
+  preset: "ts-jest",
+  testEnvironment: "node",
+  extensionsToTreatAsEsm: [".ts"],
+  transform: {
+    "^.+\\.[tj]s$": [
+      "ts-jest",
+    ],
+  },
+  transformIgnorePatterns: [""],
 };
-
 export default jestConfig;
