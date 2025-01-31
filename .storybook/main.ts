b20967ec897c5f3e9219274ff6821a7adf84bace
Paul Gottschling <paul.gottschling@goteleport.com>
2025-01-31 12:06:33 -0500

    Fix storybook config issue

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,22 +1,38 @@
-import type { StorybookConfig } from '@storybook/react';
+import type { StorybookConfig } from "@storybook/react";
 
+// Explanation of the framework config:
+// https://storybook.js.org/docs/configure/integration/compilers#the-swc-compiler-doesnt-work-with-react
 const config: StorybookConfig = {
-  // Required
   framework: {
-    name: '@storybook/react-webpack5',
-    options: {}
+    name: "@storybook/react-webpack5",
+    options: {
+      builder: {
+        useSWC: true,
+      },
+    },
   },
-
-  stories: ['../src/**/*.mdx', '../src/**/*.stories.@(js|jsx|mjs|ts|tsx)'],
-  addons: ['@storybook/addon-essentials', '@storybook/addon-webpack5-compiler-swc'],
+  swc: () => ({
+    jsc: {
+      transform: {
+        react: {
+          runtime: "automatic",
+        },
+      },
+    },
+  }),
+  stories: ["../src/**/*.stories.@(js|jsx|mjs|ts|tsx)"],
+  addons: [
+    "@storybook/addon-essentials",
+    "@storybook/addon-webpack5-compiler-swc",
+  ],
 
   docs: {},
 
   staticDirs: [],
 
   typescript: {
-    reactDocgen: 'react-docgen-typescript'
-  }
+    reactDocgen: "react-docgen-typescript",
+  },
 };
 
 export default config;
