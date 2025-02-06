c58bc3ebeb9df5808ffc42ec4e70c10d5801d0f2
Paul Gottschling <paul.gottschling@goteleport.com>
2025-02-06 10:18:08 -0500

    Fix the SVG module declaration
    Don't limit it to a string

--- .storybook/@types/imports.d.ts
+++ .storybook/@types/imports.d.ts
@@ -4,10 +4,7 @@
 
 declare module "*.css";
 
-declare module "*.svg" {
-  const value: string;
-  export default value;
-}
+declare module "*.svg";
 
 declare module "*.svg?react" {
   const Component: React.StatelessComponent<React.SVGAttributes<SVGElement>>;
