1537121aeb0ac1d0e38ffd86ffec32ae6efc62ca
Paul Gottschling <paul.gottschling@goteleport.com>
2025-03-07 14:53:15 -0500

    Add a failing test case
    This requires making some type properties optional

--- server/config-docs.test.ts
+++ server/config-docs.test.ts
@@ -8,15 +8,51 @@ import {
 describe("makeDocusaurusNavigationCategory", () => {
   interface testCase {
     description: string;
-    version: string;
     category: NavigationCategory;
     expected: DocusaurusCategory;
   }
 
-  const testCases = [];
+  const testCases: Array<testCase> = [
+    {
+      description: "Non-generated",
+      category: {
+        icon: "connect",
+        title: "User Guides",
+        entries: [
+          {
+            title: "Introduction",
+            slug: "/ver/18.x/connect-your-client/introduction/",
+          },
+          {
+            title: "Using tsh",
+            slug: "/ver/18.x/connect-your-client/tsh/",
+          },
+        ],
+      },
+      expected: {
+        collapsible: true,
+        link: { type: "doc", id: "connect-your-client/connect-your-client" },
+        items: [
+          {
+            id: "connect-your-client/introduction",
+            label: "Introduction",
+            type: "doc",
+          },
+          {
+            id: "connect-your-client/tsh",
+            label: "Using tsh",
+            type: "doc",
+          },
+        ],
+        label: "User Guides",
+        type: "category",
+      },
+    },
+    //TODO: category that uses generateFrom
+  ];
 
   test.each(testCases)("$description", (c) => {
-    expect(makeDocusaurusNavigationCategory(c.category, c.version)).toEqual(
+    expect(makeDocusaurusNavigationCategory(c.category, "18.x")).toEqual(
       c.expected
     );
   });
