52f647281eaffdb30439ba3454297d9d693d944a
Taras Postument <taras.postument@goteleport.com>
2025-03-18 10:33:30 +0000

    Revert "Test against goteleport.dev"
    This reverts commit cdb89c42ea329b4b425df060e825f2576ead0c3c.

--- src/components/Search/InkeepSearch.tsx
+++ src/components/Search/InkeepSearch.tsx
@@ -36,7 +36,7 @@ export function InkeepSearch() {
     apiKey: inkeepConfig.apiKey || '',
     organizationDisplayName: "Teleport",
     primaryBrandColor: "#512FC9",
-    aiApiBaseUrl: "https://goteleport.dev/inkeep-proxy",
+    aiApiBaseUrl: "https://goteleport.com/inkeep-proxy",
     privacyPreferences: {
       optOutAllAnalytics: true,
     },
