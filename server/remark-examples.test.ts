14c8e57981f0595a3ad91104b0dc569e0475a366
Paul Gottschling <paul.gottschling@goteleport.com>
2025-03-25 09:41:42 -0400

    Get the test to fail

--- server/remark-examples.test.ts
+++ server/remark-examples.test.ts
@@ -1,18 +1,15 @@
 import { describe, expect, test } from "@jest/globals";
-
 import { VFile, VFileOptions } from "vfile";
-import { readFileSync } from "fs";
-import { resolve } from "path";
 import { remark } from "remark";
 import mdx from "remark-mdx";
-import remarkExamples from "../server/remark-examples";
+import remarkExamples from "./remark-examples";
 
 const transformer = (vfileOptions: VFileOptions) => {
   const file: VFile = new VFile(vfileOptions);
 
   return remark()
     .use(mdx as any)
-    .use(remarkExamples as any)
+    .use(remarkExamples as any, "15.x")
     .processSync(file as any);
 };
 
@@ -23,11 +20,30 @@ describe("server/remark-examples", () => {
     expected: string;
   }
 
-  const testCases: Array<testCase> = [];
+  const testCases: Array<testCase> = [
+    {
+      description: "import statement at the beginning of the file",
+      input: `---
+title: My page
+description: My page
+---
+      import CodeExample from "@examples/access-plugin-minimal/config.go
+
+This is a paragraph.`,
+      expected: `---
+title: My page
+description: My page
+---
+
+import CodeExample from "!!raw-loader!@site/content/15.x/examples/access-plugin-minimal/config.go/access-plugin-minimal/config.go
+
+This is a paragraph.`,
+    },
+  ];
 
   test.each(testCases)("$description", (tc) => {
     const result = transformer({
-      value,
+      value: tc.input,
       path: "/content/4.0/docs/pages/filename.mdx",
     }).toString();
 
