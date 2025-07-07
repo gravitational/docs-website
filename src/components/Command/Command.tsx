b82b8bbbf4393208407025bdf087f6c74d3fd83a
Paul Gottschling <paul.gottschling@goteleport.com>
2025-07-07 10:47:34 -0400

    Emit gtag events on code copy button clicks
    Help understand how documentation readers use docs pages to perform
    operations on a Teleport cluster by adding an anonymous gtag event when
    a user clicks a code snippet copy button. This includes both the large
    button that copies an entire multi-line snippet and the smaller button
    that appears next to a single command within a `code` snippet.
    
    To begin with, gtag event payloads are minimal and contain no
    information about the content of the snippet:
    - `label`: the language of the code snippet, e.g., `code` or `yaml`
    - `scope`: whether the copy button is for a single line or the whole
      snippet
    - Default fields such as the path of the page.
    
    As part of this work, include an optional `gtag` prop in components that
    emit a gtag so we can easily and explicitly assert against gtag events
    in tests.

--- src/components/Command/Command.tsx
+++ src/components/Command/Command.tsx
@@ -3,6 +3,7 @@ import Icon from "/src/components/Icon";
 import HeadlessButton from "/src/components/HeadlessButton";
 import { toCopyContent } from "/utils/general";
 import styles from "./Command.module.css";
+import { makeCodeSnippetGtagEvent, logGtag } from "/src/utils/gtag";
 
 const TIMEOUT = 1000;
 
@@ -24,13 +25,18 @@ export function CommandComment(props: CommandCommentProps) {
 
 export interface CommandProps {
   children: ReactNode;
+  gtag?: (command: string, name: string, params: any) => {};
 }
 
-export default function Command({ children, ...props }: CommandProps) {
+export default function Command({ children, gtag, ...props }: CommandProps) {
   const [isCopied, setIsCopied] = useState<boolean>(false);
   const codeRef = useRef<HTMLDivElement>();
 
   const handleCopy = useCallback(() => {
+    const { name, params } = makeCodeSnippetGtagEvent("line", "code");
+    const gtagFn = gtag || window.gtag || logGtag;
+    gtagFn("event", name, params);
+
     if (!navigator.clipboard) {
       return;
     }
