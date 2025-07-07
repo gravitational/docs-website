e4729558d2b1a1c1a59c921437698add96b07be3
Paul Gottschling <paul.gottschling@goteleport.com>
2025-07-07 10:47:34 -0400

    Emit gtag events on code copy button clicks
    Help understand how documentation readers use docs pages to perform
    operations on a Teleport cluster by adding an anonymous gtag event when
    a user clicks a code snippet copy button. This includes both the large
    button that copies an entire multi-line snippet and the smaller button
    that appears next to a single command within a `code` snippet.
    
    To begin with, gtag event payloads are minimal and contain no
    information about the content of the snippet:
    - `label`: the language of the code snippet, e.g., `code` or `yaml`
    - `scope`: whether the copy button is for a single line or the whole
      snippet
    - Default fields such as the path of the page.
    
    As part of this work, include an optional `gtag` prop in components that
    emit a gtag so we can easily and explicitly assert against gtag events
    in tests.

--- src/theme/MDXComponents/Pre.stories.tsx
+++ src/theme/MDXComponents/Pre.stories.tsx
@@ -4,9 +4,11 @@ import { expect } from "@storybook/test";
 import { default as Pre } from "./Pre";
 import { replaceClipboardWithCopyBuffer } from "/src/utils/clipboard";
 import { Var } from "/src/components/Variables/Var";
+import { VarsProvider } from "/src/components/Variables/context";
+import { collectGtagCalls } from "/src/utils/gtag";
 
 export const SimplePre = () => (
-  <Pre>
+  <Pre gtag={collectGtagCalls()}>
     <code className="hljs language-yaml">
       <span className="hljs-attr">key1:</span>{" "}
       <span className="hljs-string">value</span>
@@ -61,42 +63,55 @@ key3:
 export const CopyVarInPre: Story = {
   render: () => {
     return (
-      <Pre>
-        <code className="hljs language-yaml">
-          <span className="hljs-attr">key1:</span>{" "}
-          <span className="hljs-string">value</span>
-          {"\n"}
-          <span className="hljs-attr">key2:</span> <Var name="value" />
-          {"\n"}
-          <span className="hljs-attr">key3:</span>
-          {"\n "}
-          <span className="hljs-bullet">-</span>{" "}
-          <span className="hljs-string">value</span>
-          {"\n "}
-          <span className="hljs-bullet">-</span>{" "}
-          <span className="hljs-string">value2</span>
-          {"\n "}
-          <span className="hljs-bullet">-</span>{" "}
-          <span className="hljs-string">value3</span>
-        </code>
-      </Pre>
+      <VarsProvider>
+        <Pre gtag={collectGtagCalls()}>
+          <code className="hljs language-yaml">
+            <span className="hljs-attr">key1:</span>{" "}
+            <span className="hljs-string">value</span>
+            {"\n"}
+            <span className="hljs-attr">key2:</span> <Var name="value" />
+            {"\n"}
+            <span className="hljs-attr">key3:</span>
+            {"\n "}
+            <span className="hljs-bullet">-</span>{" "}
+            <span className="hljs-string">value</span>
+            {"\n "}
+            <span className="hljs-bullet">-</span>{" "}
+            <span className="hljs-string">value2</span>
+            {"\n "}
+            <span className="hljs-bullet">-</span>{" "}
+            <span className="hljs-string">value3</span>
+          </code>
+        </Pre>
+      </VarsProvider>
     );
   },
   play: async ({ canvasElement, step }) => {
     replaceClipboardWithCopyBuffer();
     const canvas = within(canvasElement);
 
+    await step("Enter a variable value", async () => {
+      await userEvent.type(canvas.getByTestId("var-input"), "my-value");
+    });
+
     await step("Copy the content", async () => {
       await userEvent.hover(canvas.getByText("value3"));
       await userEvent.click(canvas.getByTestId("copy-button-all"));
       expect(navigator.clipboard.readText()).toEqual(
         `key1: value
-key2: value
+key2: my-value
 key3:
  - value
  - value2
  - value3`,
       );
+      expect(window.gtagCalls).toHaveLength(1);
+      expect(window.gtagCalls[0].params).toEqual({
+        label: "yaml",
+        // Snippet-level copy button, rather than the copy button for a code
+        // line
+        scope: "snippet",
+      });
     });
   },
 };
