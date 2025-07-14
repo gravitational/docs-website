54914dfe305d3ba8f53f8667a97dc04f678daed7
Travis Rodgers <travis.rodgers@goteleport.com>
2025-07-14 11:39:34 -0400

    adding back page path

--- src/utils/analytics.ts
+++ src/utils/analytics.ts
@@ -4,6 +4,8 @@
 interface AnalyticsEvent {
   /** The specific action that occurred (e.g., 'docs_feedback_thumbs_up', 'docs_feedback_comment_thumbs_down') */
   event_name: string;
+  /** Page path where the event occurred. Defaults to current page, but it is not always added to event. This makes sure it is.  */
+  page_path?: string;
   /** Additional custom data to track with the event */
   custom_parameters?: Record<string, any>;
 }
@@ -11,6 +13,7 @@ interface AnalyticsEvent {
 export const trackEvent = (eventData: AnalyticsEvent): void => {
   if (typeof window !== 'undefined' && window.gtag) {
     window.gtag('event', eventData.event_name, {
+      page_path: eventData.page_path || window.location.pathname,
       ...eventData.custom_parameters
     });
   }
