aea4a6b4d3818624c8c72659539272bf5fadd094
Paul Gottschling <paul.gottschling@goteleport.com>
2025-08-07 12:46:09 -0400

    Respond to taraspos feedback
    Do not include a .gitmodules configuration in the docs-website project.
    Instead, include a .gitmodules.example file that users can copy for
    local development. In production builds, only fetch releases from GitHub
    based on `content` directory names.
    
    One consequence of this approach is that we can no longer include the
    unreleased major version within the docs site, so this change removes it
    from `config.json` and `.gitmodules`.

--- scripts/checkout-latest-tag.sh
+++ scripts/checkout-latest-tag.sh
@@ -1,31 +1,36 @@
 #!/bin/bash
 set -o pipefail;
-# This script is intended to be run with git submodule foreach. It assumes that
-# the name of the given submodule is content/v<MAJOR_VERSION>.x, e.g.,
-# content/v17.x. If it cannot check out the latest release for a submodule, it
-# exits with no error, since we can use the latest commit for that submodule
-# instead (e.g., the master branch).
+# This script assumes that the name of each first-level child directory in
+# content is content/v<MAJOR_VERSION>.x, e.g., content/v17.x.
+# $1 is the relative path of the content directory to the project root.
 
-if [[ -n $(find ./ -name "docs") ]]; then
-    echo "This subodule directory already includes a docs directory. Make sure you haven't loaded the submodule";
-fi
-
-SUBMODULE_NAME='content/([0-9]+)\.x'
-# Extract the major version from the submodule name if it follows the expected
-# format.
-export MAJOR=$(echo "${name}" | grep -oE "$SUBMODULE_NAME" | sed -E "s|^${SUBMODULE_NAME}|\1|");
-if [[ "$?" -ne 0 ]]; then
-    echo "Cannot check out the latest release within a submodule: \"${name}\" does not have the expected submodule name format. Skipping.";
+if [[ -n $(find "$1" -name "docs") ]]; then
+    echo "Content directory $1 already includes a docs directory. Skipping."
     exit 0;
 fi
 
+echo "Setting up docs version in content directory $1";
+
+CONTENT_DIR_NAME='content/([0-9]+)\.x'
+# Extract the major version from the submodule name if it follows the expected
+# format.
+export MAJOR=$(echo "${1}" | grep -oE "$CONTENT_DIR_NAME" | sed -E "s|^${CONTENT_DIR_NAME}|\1|");
+if [[ "$?" -ne 0 ]]; then
+    echo "Cannot check out the latest release within a content version directory: \"${1}\" does not have the expected name format. Skipping.";
+    exit 0;
+fi
+
+echo "Found major version $MAJOR";
+
 LATEST_RELEASE_FOR_MAJOR=$(curl -qsf 'https://api.github.com/repos/gravitational/teleport/releases' | \
-    jq '.[].tag_name | select(startswith("v\(env.MAJOR)"))' | head -n 1);
+    jq -r '.[].tag_name | select(startswith("v\(env.MAJOR)"))' | head -n 1);
 
 if [[ -z "$LATEST_RELEASE_FOR_MAJOR" ]]; then
-    echo "Cannot check out the latest release within a submodule: cannot find a release for major version ${MAJOR}. Skipping.";
+    echo "Cannot check out the latest release within content directory ${1}: cannot find a release for major version ${MAJOR}. Skipping.";
     exit 0
 fi
+
+echo "Configuring the docs to display release ${LATEST_RELEASE_FOR_MAJOR}.";
 
 jq --arg major "${MAJOR}" \
    --arg release "${LATEST_RELEASE_FOR_MAJOR}" \
@@ -39,11 +44,16 @@ jq --arg major "${MAJOR}" \
              "isDefault": $isDefault,
          }
      ]
-   }' "${toplevel}/config.json" > "${toplevel}/confignew.json";
+   }' "config.json" > "confignew.json";
 
-mv "${toplevel}/confignew.json" "${toplevel}/config.json";
+mv "confignew.json" "config.json";
 
-curl -L -o release.tar "https://api.github.com/repos/gravitational/teleport/tarball/v${LATEST_RELEASE_FOR_MAJOR}";
-tar -xv --include "gravitational-teleport-*/docs/*" --include "gravitational-teleport-*/examples/*" -f release.tar
-mv gravitational-teleport-*/docs docs
-mv gravitational-teleport-*/examples examples
+RELEASE_URL="https://api.github.com/repos/gravitational/teleport/tarball/${LATEST_RELEASE_FOR_MAJOR}";
+
+echo "Fetching a release archive from ${RELEASE_URL};"
+
+curl -L -o "$1/release.tar" "${RELEASE_URL}";
+tar -xvf "$1/release.tar";
+mv gravitational-teleport-*/docs "$1/docs";
+mv gravitational-teleport-*/examples "$1/examples";
+rm -rf gravitational-teleport-*;
--- scripts/update_submodules.sh
+++ scripts/update_submodules.sh
@@ -1,36 +1,9 @@
 #!/bin/bash
 
-# $1 is the relative path to a submodule
-function fetch_submodule_if_needed () {
-  # There is already a docs directory, so there is no need to fetch a submodule.
-  if [[ -n $(find "$1" -name "docs" -type d) ]]; then
-    return
-  fi
-
-  let "i=0";
-  let "s=0";
-  while [ ${i} -lt 5 ]; do
-      git submodule update --init --remote --progress --depth 1 --single-branch "$1";      
-      if [[ "$?" -eq 0 ]]; then
-      	  return
-      fi
-
-      let "i++";
-      let "s=s+5";
-      echo "Failed to load submodules. Trying again in ${s}s."
-      sleep ${s};
-  done;
-  echo "Failed to load submodule ${1} after ${i} attempts. Exiting.";
-  exit 1;
-}
-
 if [[ -n ${AWS_APP_ID} ]]; then
-  # Attempt to download a release for each submodule.
-  git submodule foreach '../../scripts/checkout-latest-tag.sh';
- 
   # For any submodule directories where we couldn't download a release, load the
   # git submodule.
-  find content -mindepth 1 -maxdepth 1 -type d exec fetch_submodule_if_needed {} \;
+  find content -mindepth 1 -maxdepth 1 -type d -exec  'scripts/checkout-latest-tag.sh' {} \;
 else
   # This is a local machine, so load all submodules for development
   git submodule update --init --remote --progress;
