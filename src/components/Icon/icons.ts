30b8c0f74980076240a94d11646f916ba9f2d5f0
marie.mcallister <marie.mcallister@goteleport.com>
2025-08-06 19:37:44 -0700

    new componenets for ci/cd mwi page

--- src/components/Icon/icons.ts
+++ src/components/Icon/icons.ts
@@ -69,6 +69,8 @@ export { default as gitlab } from "./svg/gitlab.svg";
 export { default as circleci } from "./svg/circleci.svg";
 export { default as bitbucket } from "./svg/bitbucket.svg";
 export { default as jenkins } from "./svg/jenkins.svg";
+export { default as githubActions } from "./svg/bitbucket.svg";
+export { default as spacelifts } from "./svg/jenkins.svg";
 
 // Teleport svgs
 export { default as cluster } from "./teleport-svg/cluster.svg";
