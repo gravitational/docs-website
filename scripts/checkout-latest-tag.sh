6798929eb31200955a67fa22176d5649265d2ec1
Taras Postument <taras.postument@goteleport.com>
2025-08-08 11:32:05 +0100

    Script improvements: - Use separate downloads folder so it can be cached later - Use tar to unpack directly into target location

--- scripts/checkout-latest-tag.sh
+++ scripts/checkout-latest-tag.sh
@@ -11,19 +11,16 @@ fi
 
 echo "Setting up docs version in content directory $1";
 
+DOWNLOADS_DIR='.downloads/'
 CONTENT_DIR_NAME='content/([0-9]+)\.x'
 # Extract the major version from the submodule name if it follows the expected
 # format.
-export MAJOR=$(echo "${1}" | grep -oE "$CONTENT_DIR_NAME" | sed -E "s|^${CONTENT_DIR_NAME}|\1|");
-if [[ "$?" -ne 0 ]]; then
-    echo "Cannot check out the latest release within a content version directory: \"${1}\" does not have the expected name format. Skipping.";
-    exit 0;
-fi
 
+MAJOR=$(echo "${1}" | grep -oE "$CONTENT_DIR_NAME" | sed -E "s|^${CONTENT_DIR_NAME}|\1|")
 echo "Found major version $MAJOR";
 
 LATEST_RELEASE_FOR_MAJOR=$(curl -qsf 'https://api.github.com/repos/gravitational/teleport/releases' | \
-    jq -r '.[].tag_name | select(startswith("v\(env.MAJOR)"))' | head -n 1);
+    jq -r --arg major "$MAJOR" '.[].tag_name | select(startswith("v\($major)"))' | head -n 1);
 
 if [[ -z "$LATEST_RELEASE_FOR_MAJOR" ]]; then
     echo "Cannot check out the latest release within content directory ${1}: cannot find a release for major version ${MAJOR}. Skipping.";
@@ -49,11 +46,20 @@ jq --arg major "${MAJOR}" \
 mv "confignew.json" "config.json";
 
 RELEASE_URL="https://api.github.com/repos/gravitational/teleport/tarball/${LATEST_RELEASE_FOR_MAJOR}";
+RELEASE_TAR_FILE="${DOWNLOADS_DIR}/${LATEST_RELEASE_FOR_MAJOR}.tar.gz";
 
-echo "Fetching a release archive from ${RELEASE_URL};"
+mkdir -p "${DOWNLOADS_DIR}";
+if [[ -f "${RELEASE_TAR_FILE}" ]]; then
+    du -h "${RELEASE_TAR_FILE}";
+    echo "Archive ${RELEASE_TAR_FILE} already exists. Skipping download."
+else
+    echo "Fetching a release archive from ${RELEASE_URL} to ${RELEASE_TAR_FILE};"
+    curl -fL -o "${RELEASE_TAR_FILE}" "${RELEASE_URL}";
+fi
 
-curl -L -o "$1/release.tar" "${RELEASE_URL}";
-tar -xvf "$1/release.tar";
-mv gravitational-teleport-*/docs "$1/docs";
-mv gravitational-teleport-*/examples "$1/examples";
-rm -rf gravitational-teleport-*;
+tar -xf "${RELEASE_TAR_FILE}" \
+    --strip-components=1 \
+    -C "$1" \
+        '*/docs' '*/examples'
+
+ls -al "$1"
