d25a9334da66f7ec724c81ced62a2fd4657ac16b
Aatu Väisänen <aatu.vaisanen.ext@goteleport.com>
2025-08-13 10:24:44 +0300

    Utilize react-helmet to set full page width directly via .mdx document, remove unnecessary swizzled components

--- src/theme/DocRoot/Layout/index.tsx
+++ src/theme/DocRoot/Layout/index.tsx
@@ -2,7 +2,6 @@ import React, { type ReactNode } from "react";
 import Layout from "@theme-original/DocRoot/Layout";
 import type { WrapperProps } from "@docusaurus/types";
 import DocsNavigation from "@site/src/components/DocsNavigation";
-import "./styles.module.css";
 
 type Props = WrapperProps<any>;
 
