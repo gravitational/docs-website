ecabe10e48f4c4a3336c3bfec0805eac61c44cd0
Aatu Väisänen <aatu.vaisanen.ext@goteleport.com>
2025-08-15 13:06:31 +0300

    Revert "Use sidebar items as content source for DocsNavigation"
    This reverts commit 33786c25989f7e9a4a03daae26bb5adb87596831.

--- src/components/DocsNavigation/DocsNavigation.tsx
+++ src/components/DocsNavigation/DocsNavigation.tsx
@@ -1,14 +1,14 @@
-import { useDocsSidebar } from "@docusaurus/plugin-content-docs/client";
 import styles from "./DocsNavigation.module.css";
-import { PropSidebarItem } from "@docusaurus/plugin-content-docs";
-import { SidebarItemLink } from "@docusaurus/plugin-content-docs/src/sidebars/types.js";
-import { useLocation } from "@docusaurus/router";
 import DocsNavList from "./DocsNavList";
 
-const DocsNavigation: React.FC = () => {
-  const sidebar = useDocsSidebar();
-  const location = useLocation();
-  const items: Array<PropSidebarItem | SidebarItemLink> = sidebar ? sidebar.items : [];
+type DocsNavigationProps = {
+  items: Array<{
+    label: string;
+    href: string;
+  }>;
+};
+
+const DocsNavigation: React.FC<DocsNavigationProps> = ({ items }) => {
   return (
     <nav id="docs-navigation" className={styles.docsNavigation}>
       <DocsNavList items={items} location={location} />
