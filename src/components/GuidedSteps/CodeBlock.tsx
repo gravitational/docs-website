e2474a45c4868586ce08e21d8d505d6089697677
Aatu Väisänen <aatu.vaisanen.ext@goteleport.com>
2025-09-16 15:46:11 +0300

    Scroll mechanics

--- src/components/GuidedSteps/CodeBlock.tsx
+++ src/components/GuidedSteps/CodeBlock.tsx
@@ -1,28 +1,34 @@
-import { useImperativeHandle, useRef, forwardRef } from "react";
+import { useImperativeHandle, useRef, forwardRef, useContext } from "react";
 import styles from "./File.module.css";
 import { CodeBlockProps, CodeBlockHandle } from "./types";
+import { GuidedStepsContext } from "./context";
 
-const CodeBlock = forwardRef<CodeBlockHandle, Pick<CodeBlockProps, "children">>(
-  ({ children }, ref) => {
-    const stepRef = useRef<HTMLSpanElement | null>(null);
+const CodeBlock = forwardRef<
+  CodeBlockHandle,
+  Pick<CodeBlockProps, "children" | "fileName">
+>(({ fileName, children }, ref) => {
+  const { activeFileName, setActiveFileName } = useContext(GuidedStepsContext);
+  const stepRef = useRef<HTMLSpanElement | null>(null);
 
-    useImperativeHandle(
-      ref,
-      (): CodeBlockHandle => ({
-        activate: (): void =>
-          stepRef.current?.classList.add(styles.activeLines),
-        deactivate: (): void =>
-          stepRef.current?.classList.remove(styles.activeLines),
-      })
-    );
+  useImperativeHandle(
+    ref,
+    (): CodeBlockHandle => ({
+      activate: (): void => {
+        if (activeFileName !== fileName) setActiveFileName(fileName);
+        stepRef.current?.classList.add(styles.activeLines);
+        stepRef.current?.scrollIntoView({ behavior: "smooth", block: "nearest", inline: "center",  });
+      },
+      deactivate: (): void =>
+        stepRef.current?.classList.remove(styles.activeLines),
+    })
+  );
 
-    return (
-      <span className={styles.codeBlock} ref={stepRef}>
-        {children}
-      </span>
-    );
-  }
-);
+  return (
+    <span className={styles.codeBlock} ref={stepRef}>
+      {children}
+    </span>
+  );
+});
 
 CodeBlock.displayName = "CodeBlock";
 
