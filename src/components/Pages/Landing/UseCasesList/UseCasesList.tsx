aa7ea9dce13ab96d9c13209754677b443c3abce2
Aatu Väisänen <aatu.vaisanen.ext@goteleport.com>
2025-10-08 12:55:24 +0300

    Change heading level on doc variant

--- src/components/Pages/Landing/UseCasesList/UseCasesList.tsx
+++ src/components/Pages/Landing/UseCasesList/UseCasesList.tsx
@@ -34,17 +34,21 @@ const UseCasesList: React.FC<UseCasesListProps> = ({
   useCases = [],
   variant = "landing",
 }) => {
+  const Heading = variant === "doc" ? "h3" : "h2";
+
   return (
     <section className={cn(styles.useCasesList, className)}>
       <div className={styles.container}>
         <div className={styles.header}>
-          <h2
-            className={cn(styles.title, {
-              [styles.docVariant]: variant === "doc",
-            })}
-          >
-            {title}
-          </h2>
+          {title && (
+            <Heading
+              className={cn(styles.title, {
+                [styles.docVariant]: variant === "doc",
+              })}
+            >
+              {title}
+            </Heading>
+          )}
           {description && <p className={styles.description}>{description}</p>}
         </div>
         <ul
@@ -55,87 +59,90 @@ const UseCasesList: React.FC<UseCasesListProps> = ({
             } as React.CSSProperties
           }
         >
-          {useCases.map((caseItem, index) => (
-            <li key={index}>
-              {caseItem.href &&
-              (!caseItem.tags || caseItem.tags.length === 0) ? (
-                // @ts-ignore
-                <Link className={styles.item} to={caseItem.href}>
-                  <h3>{caseItem.title}</h3>
-                  <p
-                    className={cn(styles.description, {
-                      [styles.docVariant]: variant === "doc",
-                    })}
-                  >
-                    {caseItem.description}
-                  </p>
-                </Link>
-              ) : (
-                <div
-                  className={cn(styles.item, {
-                    [styles.hasTags]: caseItem.tags?.length > 0,
-                  })}
-                >
-                  {caseItem.href ? (
-                    // @ts-ignore
-                    <Link className={styles.linkTitle} to={caseItem.href}>
-                      <h3>{caseItem.title}</h3>
-                    </Link>
-                  ) : (
-                    <h3>{caseItem.title}</h3>
-                  )}
-                  <p
-                    className={cn(styles.description, {
+          {useCases.map((caseItem, index) => {
+            const UseCaseHeading = variant === "doc" && title ? "h4" : "h3";
+            return (
+              <li key={index}>
+                {caseItem.href &&
+                (!caseItem.tags || caseItem.tags.length === 0) ? (
+                  // @ts-ignore
+                  <Link className={styles.item} to={caseItem.href}>
+                    <UseCaseHeading>{caseItem.title}</UseCaseHeading>
+                    <p
+                      className={cn(styles.description, {
+                        [styles.docVariant]: variant === "doc",
+                      })}
+                    >
+                      {caseItem.description}
+                    </p>
+                  </Link>
+                ) : (
+                  <div
+                    className={cn(styles.item, {
                       [styles.hasTags]: caseItem.tags?.length > 0,
-                      [styles.docVariant]: variant === "doc",
                     })}
                   >
-                    {caseItem.description}
-                  </p>
-                  {caseItem.tags?.length > 0 && (
-                    <ul className={styles.tags}>
-                      {caseItem.tags.map((tag, tagIndex) => (
-                        <li key={tagIndex}>
-                          {tag.href ? (
-                            // @ts-ignore
-                            <Link className={styles.tag} to={tag.href}>
-                              {tag.icon && (
-                                <Icon
-                                  name={tag.icon}
-                                  size="md"
-                                  className={styles.tagIcon}
-                                />
-                              )}
-                              {tag.name}
-                              {tag.arrow && (
-                                // @ts-ignore
-                                <ArrowRightSvg className={styles.tagArrow} />
-                              )}
-                            </Link>
-                          ) : (
-                            <span className={styles.tag}>
-                              {tag.icon && (
-                                <Icon
-                                  name={tag.icon}
-                                  size="md"
-                                  className={styles.tagIcon}
-                                />
-                              )}
-                              {tag.name}
-                              {tag.arrow && (
-                                // @ts-ignore
-                                <ArrowRightSvg className={styles.tagArrow} />
-                              )}
-                            </span>
-                          )}
-                        </li>
-                      ))}
-                    </ul>
-                  )}
-                </div>
-              )}
-            </li>
-          ))}
+                    {caseItem.href ? (
+                      // @ts-ignore
+                      <Link className={styles.linkTitle} to={caseItem.href}>
+                        <UseCaseHeading>{caseItem.title}</UseCaseHeading>
+                      </Link>
+                    ) : (
+                      <UseCaseHeading>{caseItem.title}</UseCaseHeading>
+                    )}
+                    <p
+                      className={cn(styles.description, {
+                        [styles.hasTags]: caseItem.tags?.length > 0,
+                        [styles.docVariant]: variant === "doc",
+                      })}
+                    >
+                      {caseItem.description}
+                    </p>
+                    {caseItem.tags?.length > 0 && (
+                      <ul className={styles.tags}>
+                        {caseItem.tags.map((tag, tagIndex) => (
+                          <li key={tagIndex}>
+                            {tag.href ? (
+                              // @ts-ignore
+                              <Link className={styles.tag} to={tag.href}>
+                                {tag.icon && (
+                                  <Icon
+                                    name={tag.icon}
+                                    size="md"
+                                    className={styles.tagIcon}
+                                  />
+                                )}
+                                {tag.name}
+                                {tag.arrow && (
+                                  // @ts-ignore
+                                  <ArrowRightSvg className={styles.tagArrow} />
+                                )}
+                              </Link>
+                            ) : (
+                              <span className={styles.tag}>
+                                {tag.icon && (
+                                  <Icon
+                                    name={tag.icon}
+                                    size="md"
+                                    className={styles.tagIcon}
+                                  />
+                                )}
+                                {tag.name}
+                                {tag.arrow && (
+                                  // @ts-ignore
+                                  <ArrowRightSvg className={styles.tagArrow} />
+                                )}
+                              </span>
+                            )}
+                          </li>
+                        ))}
+                      </ul>
+                    )}
+                  </div>
+                )}
+              </li>
+            );
+          })}
         </ul>
       </div>
     </section>
