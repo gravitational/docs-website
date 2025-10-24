df80cddfb04bd769674f6d22c5c2479fb98104b7
Paul Gottschling <paul.gottschling@goteleport.com>
2025-10-24 10:02:31 -0400

    Add failing test case

--- server/validate-frontmatter.test.ts
+++ server/validate-frontmatter.test.ts
@@ -7,25 +7,53 @@ describe("validateFrontmatter", () => {
   interface testCase {
     description: string;
     input: Frontmatter;
-    errorRegExp: RegExp;
+    errorRegExpPattern: string;
   }
 
-  const testCases: Array<testCase> = {
-    // TODO: all fields
-    // TODO: no fields (okay)
-    // TODO: missing fields (okay)
-    // TODO: extra fields (include both in error)
-  };
+  const testCases: Array<testCase> = [
+    {
+      description: "all fields present",
+      input: {
+        title: "My page",
+        description: "Description for my page",
+        tags: ["one", "two", "three"],
+      },
+      errorRegExpPattern: "",
+    },
+    {
+      description: "no fields present",
+      input: {},
+      errorRegExpPattern: "",
+    },
+    {
+      description: "missing field",
+      input: {
+        title: "My page",
+        description: "Description for my page",
+      },
+      errorRegExpPattern: "",
+    },
+    {
+      description: "one extra field",
+      input: {
+        title: "My page",
+        description: "Description for my page",
+        labels: ["one", "two", "three"],
+      },
+      errorRegExpPattern: `labels`,
+    },
+    // TODO: two extra fields (include both in error)
+  ];
 
   test.each(testCases)("$description", (tc) => {
-    if (tc.errorRegExp == "") {
+    if (tc.errorRegExpPattern == "") {
       expect(() => {
-        validateFrontmatter(tc.input, allowedFields).not.toThrow();
-      });
+        validateFrontmatter(tc.input, allowedFields);
+      }).not.toThrow();
+    } else {
+      expect(() => {
+        validateFrontmatter(tc.input, allowedFields);
+      }).toThrow(new RegExp(tc.errorRegExpPattern));
     }
-
-    expect(() => {
-      validateFrontmatter(tc.input, allowedFields).toThrow(tc.errorRegExp);
-    });
   });
 });
--- server/validate-frontmatter.ts
+++ server/validate-frontmatter.ts
@@ -1,9 +1,8 @@
-export type Frontmatter ={
+export type Frontmatter = {
   [key: string]: unknown;
-}
+};
 
-export type KeyMap = {
-    [key: string]: 
-}
-
-export const validateFrontmatter = (frontmatter: Frontmatter, allowedFields: Array<string>) => {};
+export const validateFrontmatter = (
+  frontmatter: Frontmatter,
+  allowedFields: Array<string>,
+) => {};
