b83f17b69f6942d8e9cae9d7f8e725c8acde6ef8
Aatu Väisänen <70265716+aatuvai@users.noreply.github.com>
2025-12-18 10:12:00 +0200

    Add the page-level enterprise upsell banner (#483)
    * Add the ExclusivityBanner component
    
    * Add the banner to LandingHero
    
    * Adjust mobile responsivity for the LandingHero exclusivityBanner
    
    * Adjust mobile responsiveness in page-level ExclusivityBanner
    
    * Mark idgov tag as enterprise only
    
    * Use enterprise frontmatter field
    
    * add test branch
    
    * Undo reorganizing imports
    
    * Make banner sticky
    
    * Update copy text
    
    * Revert "add test branch"
    
    This reverts commit 51a7131eee83356fd150da757edebb57e1fc9097.

--- src/theme/DocItem/Layout/index.tsx
+++ src/theme/DocItem/Layout/index.tsx
@@ -13,10 +13,10 @@ import DocBreadcrumbs from "@theme/DocBreadcrumbs";
 import Unlisted from "@theme/ContentVisibility/Unlisted";
 import NavbarMobileSidebarToggle from "@theme/Navbar/MobileSidebar/Toggle";
 import type { Props } from "@theme/DocItem/Layout";
-import ThumbsFeedback from '@site/src/components/ThumbsFeedback';
-import { useDocTemplate } from '@site/src/hooks/useDocTemplate';
+import { useDocTemplate } from "@site/src/hooks/useDocTemplate";
 import { PositionProvider } from "/src/components/PositionProvider";
-
+import ExclusivityBanner from "@site/src/components/ExclusivityBanner";
+import ExclusivityContext from "@site/src/components/ExclusivityBanner/context";
 import styles from "./styles.module.css";
 
 interface ExtendedFrontMatter {
@@ -31,7 +31,9 @@ function useDocTOC(removeTOCSidebar: boolean) {
   const windowSize = useWindowSize();
 
   const hidden = frontMatter.hide_table_of_contents;
-  const removed = (frontMatter as ExtendedFrontMatter).remove_table_of_contents || removeTOCSidebar;
+  const removed =
+    (frontMatter as ExtendedFrontMatter).remove_table_of_contents ||
+    removeTOCSidebar;
   const canRender = !hidden && !removed && toc.length > 0;
 
   const mobile = canRender ? <DocItemTOCMobile /> : undefined;
@@ -49,9 +51,19 @@ function useDocTOC(removeTOCSidebar: boolean) {
   };
 }
 
+function usePageExclusivityBanner() {
+  const { frontMatter } = useDoc();
+
+  const exclusiveFeature = (frontMatter as any).enterprise;
+
+  return { exclusiveFeature };
+}
+
 export default function DocItemLayout({ children }: Props): JSX.Element {
-  const { hideTitleSection, removeTOCSidebar, fullWidth } = useDocTemplate();
+  const { hideTitleSection, removeTOCSidebar, fullWidth, isLandingPage } =
+    useDocTemplate();
   const docTOC = useDocTOC(removeTOCSidebar);
+  const { exclusiveFeature } = usePageExclusivityBanner();
   const {
     metadata: { unlisted },
   } = useDoc();
@@ -59,56 +71,59 @@ export default function DocItemLayout({ children }: Props): JSX.Element {
   // Add template-full-width class to main element when fullWidth template is used
   React.useEffect(() => {
     if (!fullWidth) return;
-    
-    const mainElement = document.querySelector('main');
+
+    const mainElement = document.querySelector("main");
     if (mainElement) {
-      mainElement.classList.add('template-full-width');
+      mainElement.classList.add("template-full-width");
     }
-    
+
     return () => {
-      const mainElement = document.querySelector('main');
+      const mainElement = document.querySelector("main");
       if (mainElement) {
-        mainElement.classList.remove('template-full-width');
+        mainElement.classList.remove("template-full-width");
       }
     };
   }, [fullWidth]);
 
   return (
-    <div className="row">
-      <div
-        className={clsx(
-          "col",
-          !docTOC.hidden && !docTOC.removed && styles.docItemCol,
-          fullWidth && styles.largeColumnPadding
-        )}
-      >
-        {unlisted && <Unlisted />}
-        <DocVersionBanner />
-        <div className={styles.docItemContainer}>
-          <article className={styles.alternateBreadcrumbs}>
-            {!hideTitleSection && <DocBreadcrumbs />}
-            <div className={styles.sidebar}>
-              <DocVersionBadge />
-              <NavbarMobileSidebarToggle />
-            </div>
-            {docTOC.mobile}
-            <DocItemContent>
-              <PositionProvider>{children}</PositionProvider>
-            </DocItemContent>
-            <DocItemFooter />
-          </article>
-          <DocItemPaginator />
-        </div>
-      </div>
-      {!docTOC.removed && (
-        <div className="col col--3">
-          <div className={styles.stickySidebar}>
-            <div className={styles.tocWithFeedback}>
-              <div className={styles.tocWrapper}>{docTOC.desktop}</div>
-            </div>
+    <ExclusivityContext.Provider value={{ exclusiveFeature }}>
+      {exclusiveFeature && !isLandingPage && <ExclusivityBanner />}
+      <div className="row">
+        <div
+          className={clsx(
+            "col",
+            !docTOC.hidden && !docTOC.removed && styles.docItemCol,
+            fullWidth && styles.largeColumnPadding
+          )}
+        >
+          {unlisted && <Unlisted />}
+          <DocVersionBanner />
+          <div className={styles.docItemContainer}>
+            <article className={styles.alternateBreadcrumbs}>
+              {!hideTitleSection && <DocBreadcrumbs />}
+              <div className={styles.sidebar}>
+                <DocVersionBadge />
+                <NavbarMobileSidebarToggle />
+              </div>
+              {docTOC.mobile}
+              <DocItemContent>
+                <PositionProvider>{children}</PositionProvider>
+              </DocItemContent>
+              <DocItemFooter />
+            </article>
+            <DocItemPaginator />
           </div>
         </div>
-      )}
-    </div>
+        {!docTOC.removed && (
+          <div className="col col--3">
+            <div className={styles.stickySidebar}>
+              <div className={styles.tocWithFeedback}>
+                <div className={styles.tocWrapper}>{docTOC.desktop}</div>
+              </div>
+            </div>
+          </div>
+        )}
+      </div>
+    </ExclusivityContext.Provider>
   );
 }
