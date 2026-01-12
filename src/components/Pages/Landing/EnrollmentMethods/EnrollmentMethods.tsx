e94fe542ada5e7bbd66e8022ff4dfb7905f97db6
Aatu Väisänen <70265716+aatuvai@users.noreply.github.com>
2026-01-12 14:31:26 +0200

    Applications → Cloud APIs page (#504)
    * Add a rows style variant to EnrollmentMethods
    
    * Add test branch
    
    * Change layout
    
    * Adjust hover state on the link
    
    * trigger build
    
    * adjust icon width
    
    * Revert "Add test branch"
    
    This reverts commit 604e8115bf96064d52de41a8f983ff27bb3e332b.

--- src/components/Pages/Landing/EnrollmentMethods/EnrollmentMethods.tsx
+++ src/components/Pages/Landing/EnrollmentMethods/EnrollmentMethods.tsx
@@ -1,7 +1,9 @@
+import React from "react";
 import Icon, { IconName } from "@site/src/components/Icon";
 import styles from "./EnrollmentMethods.module.css";
 import Link from "@docusaurus/Link";
 import ArrowRightSvg from "@site/src/components/Icon/teleport-svg/arrow-right.svg";
+import cn from "classnames";
 
 interface Tag {
   name: string;
@@ -17,94 +19,168 @@ interface TagList {
 
 interface EnrollmentMethod {
   title: string;
+  description?: string;
   icon: any;
   href?: string;
   tagLists: TagList[];
   children?: React.ReactNode;
+  variant?: "default" | "rows";
+  innerMethod?: boolean;
 }
 
 interface EnrollmentMethodsProps {
   title?: string;
   children?: React.ReactNode;
+  variant?: "default" | "rows";
 }
 
 export const Method: React.FC<EnrollmentMethod> = ({
   title,
+  description,
   href,
   tagLists = [],
   children,
   icon: IconComponent,
+  variant,
+  innerMethod = false,
 }) => {
   return (
-    <li className={styles.method}>
-      <IconComponent className={styles.methodIcon} />
-      <h3 className={styles.methodTitle}>
-        {href ? (
-          // @ts-ignore
-          <Link to={href} className={styles.methodLink}>
-            {title}
-          </Link>
-        ) : (
-          title
+    <div
+      className={cn(styles.method, {
+        [styles.rowsVariant]: variant === "rows",
+        [styles.innerMethodRowsVariant]: innerMethod && variant === "rows",
+      })}
+    >
+      <div
+        className={cn(styles.methodHeader, {
+          [styles.rowsVariant]: variant === "rows",
+        })}
+      >
+        {IconComponent && (
+          <IconComponent
+            className={cn(styles.methodIcon, {
+              [styles.rowsVariant]: variant === "rows",
+            })}
+          />
         )}
-      </h3>
-      <div className={styles.methodDescription}>{children}</div>
-      {tagLists.length > 0 &&
-        tagLists.map((tagList, index) => (
-          <div key={index} className={styles.tagList}>
-            {tagList.title && (
-              <h4 className={styles.tagListTitle}>{tagList.title}</h4>
+        <div>
+          <h3
+            className={cn(styles.methodTitle, {
+              [styles.innerMethodRowsVariant]:
+                innerMethod && variant === "rows",
+            })}
+          >
+            {href ? (
+              // @ts-ignore
+              <Link to={href} className={styles.methodLink}>
+                {title}
+                <span />
+              </Link>
+            ) : (
+              title
             )}
-            <ul className={styles.tags}>
-              {tagList.tags.map((tag, tagIndex) => (
-                <li key={tagIndex}>
-                  {tag.href ? (
-                    <Link className={styles.tag} to={tag.href}>
-                      {tag.icon && (
-                        <Icon
-                          name={tag.icon}
-                          size="md"
-                          className={styles.tagIcon}
-                        />
-                      )}
-                      {tag.name}
-                      {tag.arrow && (
-                        <ArrowRightSvg className={styles.tagArrow} />
-                      )}
-                    </Link>
-                  ) : (
-                    <span className={styles.tag}>
-                      {tag.icon && (
-                        <Icon
-                          name={tag.icon}
-                          size="md"
-                          className={styles.tagIcon}
-                        />
-                      )}
-                      {tag.name}
-                      {tag.arrow && (
-                        <ArrowRightSvg className={styles.tagArrow} />
-                      )}
-                    </span>
-                  )}
-                </li>
-              ))}
-            </ul>
-          </div>
-        ))}
-    </li>
+          </h3>
+          {description && (
+            <div className={styles.methodSubtitle}>{description}</div>
+          )}
+        </div>
+      </div>
+      <div>
+        <div
+          className={cn(styles.methodContent, {
+            [styles.rowsVariant]: variant === "rows" && !innerMethod,
+            [styles.innerMethodRowsVariant]: innerMethod && variant === "rows",
+          })}
+        >
+          {React.Children.map(children, (child) => {
+            if (React.isValidElement(child) && child.type === Method) {
+              return React.cloneElement(child as React.ReactElement<any>, {
+                innerMethod: true,
+                variant,
+              });
+            }
+            return child;
+          })}
+        </div>
+        {tagLists.length > 0 &&
+          tagLists.map((tagList, index) => (
+            <div key={index} className={styles.tagList}>
+              {tagList.title && (
+                <h4 className={styles.tagListTitle}>{tagList.title}</h4>
+              )}
+              <ul className={styles.tags}>
+                {tagList.tags.map((tag, tagIndex) => (
+                  <li key={tagIndex}>
+                    {tag.href ? (
+                      <Link className={styles.tag} to={tag.href}>
+                        {tag.icon && (
+                          <Icon
+                            name={tag.icon}
+                            size="md"
+                            className={styles.tagIcon}
+                          />
+                        )}
+                        {tag.name}
+                        {tag.arrow && (
+                          <ArrowRightSvg className={styles.tagArrow} />
+                        )}
+                      </Link>
+                    ) : (
+                      <span className={styles.tag}>
+                        {tag.icon && (
+                          <Icon
+                            name={tag.icon}
+                            size="md"
+                            className={styles.tagIcon}
+                          />
+                        )}
+                        {tag.name}
+                        {tag.arrow && (
+                          <ArrowRightSvg className={styles.tagArrow} />
+                        )}
+                      </span>
+                    )}
+                  </li>
+                ))}
+              </ul>
+            </div>
+          ))}
+      </div>
+    </div>
   );
 };
 
 const EnrollmentMethods: React.FC<EnrollmentMethodsProps> = ({
   title,
   children,
+  variant = "default",
 }) => {
   return (
     <section className={styles.enrollmentMethods}>
       <div className={styles.container}>
-        {title && <h2 className={styles.title}>{title}</h2>}
-        <ul className={styles.methodsList}>{children}</ul>
+        {title && (
+          <h2
+            className={cn(styles.title, {
+              [styles.rowsVariant]: variant === "rows",
+            })}
+          >
+            {title}
+          </h2>
+        )}
+        <div
+          className={cn(styles.methodsList, {
+            [styles.rowsVariant]: variant === "rows",
+          })}
+        >
+          {React.Children.map(children, (child) => {
+            if (React.isValidElement(child) && child.type === Method) {
+              return React.cloneElement(child as React.ReactElement<any>, {
+                variant,
+              });
+            }
+            return child;
+          })}
+        </div>
       </div>
     </section>
   );
