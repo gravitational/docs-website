2d6a8a9b25e37be980932f1996005cd4a6940c85
Aatu Väisänen <aatu.vaisanen.ext@goteleport.com>
2026-01-27 13:01:13 +0200

    Separate style for faq details

--- src/theme/MDXComponents/Details.tsx
+++ src/theme/MDXComponents/Details.tsx
@@ -2,6 +2,7 @@ import React, { type ReactNode } from "react";
 import Details from "@theme-original/MDXComponents/Details";
 import type DetailsType from "@theme/MDXComponents/Details";
 import type { WrapperProps } from "@docusaurus/types";
+import cn from "classnames";
 import styles from "./Details.module.css";
 
 type Props = WrapperProps<typeof DetailsType>;
@@ -9,7 +10,10 @@ type Props = WrapperProps<typeof DetailsType>;
 export default function DetailsWrapper(props: Props): ReactNode {
   return (
     <>
-      <Details {...props} className={styles.details} />
+      <Details
+        {...props}
+        className={cn(styles.details, { [styles.neutral]: !!props?.neutral })}
+      />
     </>
   );
 }
