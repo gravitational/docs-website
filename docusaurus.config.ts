e9732ad89db45f489f57bbf8d0e3bb4d1d05c4b4
Travis Swientek <tswientek@goteleport.com>
2026-03-23 10:46:36 -0500

    Adds support for Google Tag Gateway configuration.

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -49,6 +49,31 @@ function clayTrackingPlugin() {
   };
 }
 
+const GTM_CONTAINER_ID = "GTM-WMR7H6";
+
+function googleTagGatewayPlugin() {
+  return {
+    name: 'google-tag-gateway',
+    injectHtmlTags() {
+      return {
+        headTags: [
+          {
+            tagName: 'script',
+            innerHTML: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://goteleport.com/m/'+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','${GTM_CONTAINER_ID}');`,
+          },
+        ],
+        postBodyTags: [
+          `<noscript><iframe src="https://goteleport.com/m/" height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>`,
+        ],
+      };
+    },
+  };
+}
+
 const config: Config = {
   future: {
     v4: {
@@ -203,12 +228,7 @@ const config: Config = {
         },
       },
     ],
-    [
-      "@docusaurus/plugin-google-tag-manager",
-      {
-        containerId: "GTM-WMR7H6",
-      },
-    ],
+    googleTagGatewayPlugin,
     "@docusaurus/theme-classic",
     "@docusaurus/plugin-sitemap",
     [
