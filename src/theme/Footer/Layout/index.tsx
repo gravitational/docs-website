d761fb1654933c89e8bb40c45351eac988ef0a47
Aatu Väisänen <70265716+aatuvai@users.noreply.github.com>
2026-06-03 13:19:02 +0300

    Add a footer to the docs website (#673)
    * Add site footer
    
    * Fix beams link
    
    * adjust footer
    
    * drop the email input from the cta to avoid the need to duplicate form logic from marketing site
    
    * Update the footer style according to design changes
    
    * lowercase 'new'

---
+++ src/theme/Footer/Layout/index.tsx
@@ -0,0 +1,64 @@
+import React, { type ReactNode } from "react";
+import clsx from "clsx";
+import Link from "@docusaurus/Link";
+import { ThemeClassNames } from "@docusaurus/theme-common";
+import type { Props } from "@theme/Footer/Layout";
+import styles from "../Footer.module.css";
+import Button from "@site/src/components/Button/Button";
+
+export default function FooterLayout({
+  style,
+  links,
+  logo,
+  copyright,
+}: Props): ReactNode {
+  return (
+    <footer
+      className={clsx(
+        ThemeClassNames.layout.footer.container,
+        styles.footer,
+        "footer",
+        {
+          "footer--dark": style === "dark",
+        },
+      )}
+    >
+      <div className={styles.footerLayout}>
+        <div className={styles.footerCta}>
+          <div>
+            <h2 className={styles.footerCtaTitle}>
+              Stop wrangling SSH keys, VPNs, and bastion hosts
+            </h2>
+            <p>
+              Deploy a Teleport cluster, connect your servers, Kubernetes
+              clusters, databases, and apps, and replace long-lived secrets with
+              cryptographic identity and just-in-time access — all managed as
+              code. Start your free Enterprise 14-day trial today!
+            </p>
+          </div>
+          <Button
+            as="link"
+            href="https://goteleport.com/signup/"
+            className={styles.footerCtaButton}
+          >
+            Start your free trial
+          </Button>
+        </div>
+        {logo && <div className={styles.footerLogo}>{logo}</div>}
+        {links}
+        <div className={styles.footerBottom}>
+          {copyright && <div>{copyright}</div>}
+          <div className={styles.footerLegalLinks}>
+            <Link href="https://goteleport.com/legal/tos/">
+              Terms of Service
+            </Link>
+            <Link href="https://goteleport.com/legal/wtou/">
+              Website Terms of Use
+            </Link>
+            <Link href="https://goteleport.com/legal/privacy/">Privacy</Link>
+          </div>
+        </div>
+      </div>
+    </footer>
+  );
+}
