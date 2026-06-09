3fefd92cda8d4dece1e62000a8409c6bb16f4e36
Aatu Väisänen <aatu.vaisanen.ext@goteleport.com>
2026-06-09 16:17:52 +0300

    Revert swizzling DocRoot/Layout/Sidebar and DocSidebar/Desktop/Content, add role="navigation" and aria-label="Docs sidebar" to DocSidebar/Desktop

--- src/theme/DocSidebar/Desktop/index.tsx
+++ src/theme/DocSidebar/Desktop/index.tsx
@@ -23,8 +23,10 @@ function DocSidebarDesktop({ path, sidebar, onCollapse, isHidden }: Props) {
       className={clsx(
         styles.sidebar,
         hideOnScroll && styles.sidebarWithHideableNavbar,
-        isHidden && styles.sidebarHidden
+        isHidden && styles.sidebarHidden,
       )}
+      role="navigation"
+      aria-label="Docs sidebar"
     >
       <div className={styles.controlsWrapper}>
         <div className={styles.versions}>
